e354657c44d3ce765537bfed17faf83b0a4f7889
frogo123 <eddief28@tiscali.co.uk>
2018-10-01 05:13:45 +0100

    add completed algorithm to the repo

--- selectiveSortAlgorithm.js
+++ selectiveSortAlgorithm.js
@@ -1,6 +1,24 @@
 //selective sort algorithm
+function selectiveSort(arr){
 //loop 1
-
+for (let i = 0; i<arr.length-1; i++){
 //loop2
-
+    for (let j = i+1; j<arr.length; j++){
+        if (arr[i] > arr[j]){
+            swap(i, j);
+        }
+    }
+}
+return arr;
+}
 //swap function
+function swap (elementOneIndex, elementTwoIndex){
+    let temp = arr[elementOneIndex]
+    arr[elementOneIndex] = arr[elementTwoIndex];
+    arr[elementTwoIndex] = temp;
+}
+
+//arr is your array to sort
+arr = [1, 34, 45, 23, 44, 89 , 2, 3];
+
+console.log(selectiveSort(arr));
