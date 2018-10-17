baf87b73ac286b8aa71701556a6d97dfb3751b8e
frogo123 <eddief28@tiscali.co.uk>
2018-10-17 20:22:13 +0100

    add new mergeSort.js file to repo

---
+++ mergeSort.js
@@ -0,0 +1,32 @@
+//an example of merge sort algorithm
+function mergeSort(arr1, arr2) {
+    let newArr = [];
+    let i = 0;
+    let j = 0;
+    while(i != arr1.length && j != arr2.length ) {
+        if (arr1[i] < arr2[j]){
+            newArr.push(arr1[i]);
+            i++;
+        }
+        else {
+            newArr.push(arr2[j]);
+            j++
+        }
+        
+    }
+    if (i === arr1.length){
+        while(j !== arr2.length){
+        newArr.push(arr1[j]);
+        j++;
+        }
+    }
+    else {
+        while(i !== arr1.length){
+        newArr.push(arr1[i])
+        i++
+        }
+    }
+    return newArr
+}
+
+console.log(mergeSort([1, 2, 3, 4, 8, 9, 110], [6, 7]))
